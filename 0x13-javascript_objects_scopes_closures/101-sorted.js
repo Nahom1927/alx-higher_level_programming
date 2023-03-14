6224c461db5484d23368c4989d52eff7345d00a8
Nahom1927 <yheys898184@gmail.com>
2023-03-15 01:04:12 +0300

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+
+const dict = require('./101-data').dict;
+
+const newDict = {};
+
+const keys = Object.values(dict).map(String);
+const values = Object.keys(dict).map(String);
+
+for (let i = 0; i < values.length; i++) {
+  if (!newDict[keys[i]]) {
+    newDict[keys[i]] = [];
   }
+
+  newDict[keys[i]].push(values[i]);
 }
+
 console.log(newDict);
