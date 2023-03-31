b4f372066f730db370822fe71af50400cb8bf68e
Nahom1927 <yheys898184@gmail.com>
2023-03-31 13:09:57 -0700

    all

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-import urllib.request
+"""A script that
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
 
-    deco = ["utf8 content", content.decode()]
-    headers = [["type", type(content)], ["content", content], deco]
-    print("Body response:")
-    for el in headers:
-        print("\t- {}: {}".format(el[0], el[1]))
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
